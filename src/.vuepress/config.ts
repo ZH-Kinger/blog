1b0d35be57a1126d930989a4096d6aeb1c519922
王梓涵 <914132612@qq.com>
2026-02-13 19:30:01 +0800

    修改配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,4 @@
 import { defineUserConfig } from "vuepress";
-
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -10,6 +9,14 @@ export default defineUserConfig({
   description: "vuepress-theme-hope 的博客blog",
 
   theme,
+
+  // 添加以下 head 配置来解决语雀图片不显示的问题
+  head: [
+    [
+      "meta",
+      { name: "referrer", content: "no-referrer" }
+    ]
+  ],
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
